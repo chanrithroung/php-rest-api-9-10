8959620e36c1967b8c6f2e825ca10d4c8d9652ea
Chanrith <chanrithroung2@gmail.com>
2025-06-01 10:32:57 +0700

    finish

--- api.php
+++ api.php
@@ -5,7 +5,7 @@
     header("Access-Control-Allow-Origin: *");
 
     // Optional headers if needed (especially for POST, PUT, DELETE)
-    header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
+    header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type");
 
 
@@ -55,6 +55,30 @@
                 $title = $input['title'];
                 $price = $input['price'];
                 $sql = "UPDATE `products` SET `title` =  '$title',  `price` = '$price' WHERE `id` = '$id'";
+                $connection->query($sql);
+                http_response_code(200);
+                echo json_encode([
+                    'status' => 200,
+                    'message' => 'Product updated successfully!'
+                ]);
+            } else {
+                http_response_code(400);
+                echo json_encode(
+                    [
+                        'status' => 500,
+                        'message' => 'Invalid Field'
+                    ]
+                );
+            }
+        } break;
+
+
+        case 'DELETE': {
+            $input = json_decode(file_get_contents("php://input"), true);
+
+            if(isset($input['id']) ) {
+                $id = $input['id'];
+                $sql = "DELETE FROM `products` WHERE `id` = '$id';";
                 $connection->query($sql);
                 http_response_code(200);
                 echo json_encode([
